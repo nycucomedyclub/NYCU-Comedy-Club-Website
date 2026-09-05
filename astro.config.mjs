2de83ef3cd74b7213fa8f52bc66bfbec45ced332
seanhhl <sean.hanhsiang@gmail.com>
2026-09-05 22:20:43 +0800

    Add initial configuration for Jekyll site with URL and plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://comedy.nycu.cc',
+  site: 'https://comedy.nycu.cc',
+  integrations: [sitemap()],
 });
